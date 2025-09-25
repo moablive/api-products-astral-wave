d460cab9bd45e68120de47c95fa7353c97bbe212
moablive <guilhermeferrazbonato@gmail.com>
2025-09-25 00:06:15 -0300

    ajuste de rota

--- router/router.go
+++ router/router.go
@@ -8,8 +8,8 @@ import (
 )
 
 func NewRouter(authHandler *handlers.AuthHandler) *http.ServeMux {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/api/user/register", authHandler.Register)
-	mux.HandleFunc("/api/user/login", authHandler.Login)
-	return mux
+    mux := http.NewServeMux()
+    mux.HandleFunc("/user/register", authHandler.Register)
+    mux.HandleFunc("/user/login", authHandler.Login)
+    return mux
 }
